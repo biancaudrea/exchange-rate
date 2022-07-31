12dfcf6865e06c9e7c70262862af13ec4b32d6a5
unknown <bianca.udrea@gmail.com>
2022-07-31 15:54:48 +0300

    initial commit

---
+++ src/test/java/com/betvictor/exchangerate/ExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betvictor.exchangerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
